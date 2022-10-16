8db04d2305954c8ca7ffb364247c701ca895e466
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2022-10-16 20:53:48 -0300

    Verificação OAuth2 completa

--- youtube.js
+++ youtube.js
@@ -14,10 +14,10 @@ async function robot(){
         const webServer = await startWebServer()
         const OAuthClient = await createOAuthCliente()
         requestUserConsent(OAuthClient)
-        // await waitForGoogleCallback()
-        // await requestGoogleForAccessTokens()
-        // await setGlobalGoogleAuthentication()
-        // await stopWebServer()
+        const authorizationToken = await waitForGoogleCallback(webServer)
+        await requestGoogleForAccessTokens(OAuthClient, authorizationToken)
+        await setGlobalGoogleAuthentication(OAuthClient)
+        await stopWebServer(webServer)
 
         async function startWebServer(){
             return new Promise((resolve, reject) => {
@@ -55,6 +55,53 @@ async function robot(){
 
             console.log(`> Please give your consent: ${consentUrl}`)
         }
+
+        async function waitForGoogleCallback(webServer){
+            return new Promise((resolve, reject) => {
+                console.log(`> Waiting for use consent...`)
+
+                webServer.app.get('/oauth2callback', (req, res) => {
+                    const authCode = req.query.code 
+                    console.log(`> Consent given: ${authCode}`)
+
+                    res.send('<h1>Thank you!</h1><p>Now close this tab.</p>')
+                    resolve(authCode)
+                })
+            })
+        }
+
+        async function requestGoogleForAccessTokens(OAuthClient, authorizationToken){
+            return new Promise((resolve, reject) => {
+                OAuthClient.getToken(authorizationToken, (error, tokens) => {
+                    if (error){
+                        return reject(error)
+                    }
+
+                    console.log('> Access tokens received:')
+                    console.log(tokens);
+
+                    OAuthClient.setCredentials(tokens)
+                    resolve()
+                })
+            })
+        }
+
+        function setGlobalGoogleAuthentication(OAuthClient){
+            google.options({
+                auth: OAuthClient
+            })
+        }
+
+        async function stopWebServer(webServer){
+            return new Promise((resolve, reject) => {
+                webServer.server.close(() => {
+                    resolve()
+                })
+            })
+        }
+
+
+
     }
 }
 
