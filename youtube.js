ca4db03a36e17e86c26f6d93de921df2f396fb3a
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2022-10-19 00:28:19 -0300

    contador para verificar quantas vezes aquele titulo do canal se repetiu

--- youtube.js
+++ youtube.js
@@ -10,103 +10,145 @@ const { request } = require('http')
 async function robot(){
     // const content = state.load()
 
-    await authenticateWithOAuth()
-    const listHistory = await getViewingHistory()
+    // await authenticateWithOAuth()
+    // const listHistory = await getViewingHistory()
 
+    // INICIO DA AUTENTICAÇÃO OAUTH2
 
-    async function authenticateWithOAuth(){
-        const webServer = await startWebServer()
-        const OAuthClient = await createOAuthCliente()
-        requestUserConsent(OAuthClient)
-        const authorizationToken = await waitForGoogleCallback(webServer)
-        await requestGoogleForAccessTokens(OAuthClient, authorizationToken)
-        await setGlobalGoogleAuthentication(OAuthClient)
-        await stopWebServer(webServer)
+    // async function authenticateWithOAuth(){
+    //     const webServer = await startWebServer()
+    //     const OAuthClient = await createOAuthCliente()
+    //     requestUserConsent(OAuthClient)
+    //     const authorizationToken = await waitForGoogleCallback(webServer)
+    //     await requestGoogleForAccessTokens(OAuthClient, authorizationToken)
+    //     await setGlobalGoogleAuthentication(OAuthClient)
+    //     await stopWebServer(webServer)
 
-        async function startWebServer(){
-            return new Promise((resolve, reject) => {
-                const port = 5000
-                const app = express()
+    //     async function startWebServer(){
+    //         return new Promise((resolve, reject) => {
+    //             const port = 5000
+    //             const app = express()
 
-                const server = app.listen(port, () => {
-                    console.log(`> Listening on http://localhost:${port}`)
+    //             const server = app.listen(port, () => {
+    //                 console.log(`> Listening on http://localhost:${port}`)
 
-                    resolve({
-                        app,
-                        server
-                    })
-                })
-            })
-        }
+    //                 resolve({
+    //                     app,
+    //                     server
+    //                 })
+    //             })
+    //         })
+    //     }
 
-        async function createOAuthCliente(){
-            const credentials = require('./credenciais/client_secret_71860673882-fbidnfpj8je6f4ve0e1b7jfdbg3u88gu.apps.googleusercontent.com.json')
+    //     async function createOAuthCliente(){
+    //         const credentials = require('./credenciais/client_secret_71860673882-fbidnfpj8je6f4ve0e1b7jfdbg3u88gu.apps.googleusercontent.com.json')
 
-            const OAuthClient = new OAuth2(
-                credentials.web.client_id,
-                credentials.web.client_secret,
-                credentials.web.redirect_uris[0]
-            )
+    //         const OAuthClient = new OAuth2(
+    //             credentials.web.client_id,
+    //             credentials.web.client_secret,
+    //             credentials.web.redirect_uris[0]
+    //         )
 
-            return OAuthClient
-        }
+    //         return OAuthClient
+    //     }
 
-        function requestUserConsent(OAuthClient){
-            const consentUrl = OAuthClient.generateAuthUrl({
-                access_type: 'offline',
-                scope: ['https://www.googleapis.com/auth/youtube']
-            })
+    //     function requestUserConsent(OAuthClient){
+    //         const consentUrl = OAuthClient.generateAuthUrl({
+    //             access_type: 'offline',
+    //             scope: ['https://www.googleapis.com/auth/youtube']
+    //         })
 
-            console.log(`> Please give your consent: ${consentUrl}`)
-        }
+    //         console.log(`> Please give your consent: ${consentUrl}`)
+    //     }
 
-        async function waitForGoogleCallback(webServer){
-            return new Promise((resolve, reject) => {
-                console.log(`> Waiting for use consent...`)
+    //     async function waitForGoogleCallback(webServer){
+    //         return new Promise((resolve, reject) => {
+    //             console.log(`> Waiting for use consent...`)
 
-                webServer.app.get('/oauth2callback', (req, res) => {
-                    const authCode = req.query.code 
-                    console.log(`> Consent given: ${authCode}`)
+    //             webServer.app.get('/oauth2callback', (req, res) => {
+    //                 const authCode = req.query.code 
+    //                 console.log(`> Consent given: ${authCode}`)
 
-                    res.send('<h1>Thank you!</h1><p>Now close this tab.</p>')
-                    resolve(authCode)
-                })
-            })
-        }
+    //                 res.send('<h1>Thank you!</h1><p>Now close this tab.</p>')
+    //                 resolve(authCode)
+    //             })
+    //         })
+    //     }
 
-        async function requestGoogleForAccessTokens(OAuthClient, authorizationToken){
-            return new Promise((resolve, reject) => {
-                OAuthClient.getToken(authorizationToken, (error, tokens) => {
-                    if (error){
-                        return reject(error)
-                    }
+    //     async function requestGoogleForAccessTokens(OAuthClient, authorizationToken){
+    //         return new Promise((resolve, reject) => {
+    //             OAuthClient.getToken(authorizationToken, (error, tokens) => {
+    //                 if (error){
+    //                     return reject(error)
+    //                 }
 
-                    console.log('> Access tokens received:')
-                    console.log(tokens);
+    //                 console.log('> Access tokens received:')
+    //                 console.log(tokens);
 
-                    OAuthClient.setCredentials(tokens)
-                    resolve()
-                })
-            })
-        }
+    //                 OAuthClient.setCredentials(tokens)
+    //                 resolve()
+    //             })
+    //         })
+    //     }
 
-        function setGlobalGoogleAuthentication(OAuthClient){
-            google.options({
-                auth: OAuthClient
-            })
-        }
+    //     function setGlobalGoogleAuthentication(OAuthClient){
+    //         google.options({
+    //             auth: OAuthClient
+    //         })
+    //     }
 
-        async function stopWebServer(webServer){
-            return new Promise((resolve, reject) => {
-                webServer.server.close(() => {
-                    resolve()
-                })
-            })
-        }
-    }
+    //     async function stopWebServer(webServer){
+    //         return new Promise((resolve, reject) => {
+    //             webServer.server.close(() => {
+    //                 resolve()
+    //             })
+    //         })
+    //     }
+    // }
 
+    //FIM DA AUTENTICAÇÃO OAUTH2
+    // const history = require('./histórico-de-visualização.json')
     async function getViewingHistory(){
-       
+
+        //pega o arquivo json
+        const history = require('./histórico-de-visualização.json')
+        
+        //pega os subtitulos do canal - titulo e url
+        const names = history.youtube.map( x => x.subtitles)
+
+        //filtra os arquivos undefined
+        const namesNoUndefined = names.filter(x => x != undefined)
+        
+        //pega apenas o nome dos canais
+        const channel = namesNoUndefined.map(x => x[0].name)
+
+        //adiciona os titulos dos canais em uma lista e ordena
+        let list = []
+        channel.map(x => list.push(x))
+        list.sort()
+        
+        //função para criar um objeto com o titulo do canal e o númeron de ocorrencia
+        function channelYoutube(name, ocorrency){
+            this.name = name,
+            this.ocorrency = ocorrency
+        }
+
+        // Conta quantas vezes o titulo do canal apareceu
+        var current = null;
+        var cnt = 0;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i] != current) {
+                if (cnt > 0) {
+                    var newChannel = new channelYoutube(list[i-1], cnt)
+                    console.log(newChannel)
+                }
+                current = list[i];
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+        }
+
         // function execute() {
         //     return youtube.channels.list({
         //         mine: true, 
@@ -131,7 +173,7 @@ async function robot(){
         
     
     }    
-    
+    getViewingHistory()
 }
 
 module.exports = robot
